61b9885fd143fbdfe53e5f8596accc4b35614955
pappferencgabor <p.feco2005@gmail.com>
2024-10-11 10:33:51 +0200

    admin + menu

--- DuszaFogadas/Admin.xaml.cs
+++ DuszaFogadas/Admin.xaml.cs
@@ -42,8 +42,6 @@ namespace DuszaFogadas
             MySqlCommand com = new MySqlCommand("SELECT * FROM felhasznalok", conn);
             MySqlDataReader reader = com.ExecuteReader();
 
-            MessageBox.Show(reader.HasRows.ToString());
-
             if (reader.HasRows)
             {
                 while (reader.Read())
@@ -67,7 +65,91 @@ namespace DuszaFogadas
             if (selectedUser != null) 
             {
                 txtUsername.Text = selectedUser.Name;
+
+                txtPoints.Text = selectedUser.Points.ToString();
+
+                if (selectedUser.Role.ToString() == "A") { cboUserRole.Text = "Admin"; }
+                else if (selectedUser.Role.ToString() == "SZ") { cboUserRole.Text = "Szervező"; }
+                else if (selectedUser.Role.ToString() == "F") { cboUserRole.Text = "Felhasználó"; }
             }
+        }
+
+        private void btnNewUser_Click(object sender, RoutedEventArgs e)
+        {
+            Regisztracio regisztracio = new Regisztracio();
+            regisztracio.Closed += RegisterWindow_Closed;
+            regisztracio.Show();
+        }
+
+        private void btnModifyUser_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedUser = dgData.SelectedItem as User;
+
+            users[dgData.SelectedIndex].Name = txtUsername.Text;
+            users[dgData.SelectedIndex].Points = Convert.ToInt32(txtPoints.Text);
+            users[dgData.SelectedIndex].Role = UserEnum.ConvertEnum(convertRole(cboUserRole.Text.ToString()));
+
+            dgData.Items.Refresh();
+
+            MySqlConnection conn = GetMysqlConnection.getMysqlConnection();
+            conn.Open();
+            MySqlCommand com = new MySqlCommand($"UPDATE felhasznalok SET nev=@nev, pontok=@pontok, szerepkor=@szerepkor WHERE id=@id", conn);
+            com.Parameters.AddWithValue("@nev", txtUsername.Text);
+            com.Parameters.AddWithValue("@pontok", int.Parse(txtPoints.Text));
+            com.Parameters.AddWithValue("@szerepkor", convertRole(cboUserRole.Text.ToString()));
+            com.Parameters.AddWithValue("@id", selectedUser.Id);
+            com.ExecuteNonQuery();
+            conn.Close();
+
+            clearTextboxes();
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedUser = dgData.SelectedItem as User;
+            users.RemoveAt(dgData.SelectedIndex);
+
+            MySqlConnection conn = GetMysqlConnection.getMysqlConnection();
+            conn.Open();
+            MySqlCommand com = new MySqlCommand($"DELETE FROM felhasznalok WHERE id=@id", conn);
+            com.Parameters.AddWithValue("@id", selectedUser.Id);
+            com.ExecuteNonQuery();
+            conn.Close();
+
+            clearTextboxes();
+        }
+
+        private void RegisterWindow_Closed(object sender, EventArgs e)
+        {
+            users.Clear();
+            LoadPage();
+        }
+
+        private string convertRole(string role)
+        {
+            if (role == "Admin")
+            {
+                return "A";
+            }
+            else if (role == "Szervező")
+            {
+                return "SZ";
+            }
+            else if (role == "Felhasználó")
+            {
+                return "F";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    
+        private void clearTextboxes()
+        {
+            txtUsername.Clear();
+            txtPoints.Clear();
+            cboUserRole.Text = "";
         }
     }
 }
